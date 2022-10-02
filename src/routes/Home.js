743e313c70434aa40e729f522c15dead2b1c6e74
arukendev <iamjaehyun4@gmail.com>
2022-10-02 15:15:12 +0900

    create database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,36 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
 function Home() {
-  return <div>HOME</div>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={nweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
